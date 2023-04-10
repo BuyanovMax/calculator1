5943dfdb95f2ed268f9b033c44a423c31044a9f8
Макс <maxim__90.90@mail.ru>
2023-04-10 18:53:42 +0400

    final commit

--- src/main/java/ru/calcmax/calculator1/CalcController.java
+++ src/main/java/ru/calcmax/calculator1/CalcController.java
@@ -1,4 +1,41 @@
 package ru.calcmax.calculator1;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
 public class CalcController {
+    private final ru.calcmax.calculator1.CalcService calcService;
+
+    public CalcController(ru.calcmax.calculator1.CalcService calcService) {
+        this.calcService = calcService;
+    }
+
+    @GetMapping(path = "")
+    public String hello() {
+        return calcService.hello();
+    }
+
+    @GetMapping(path = "/plus")
+    public String plus(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " + " + num2 + " = " + calcService.plus(num1, num2);
+    }
+
+    @GetMapping(path = "/minus")
+    public String minus(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " - " + num2 + " = " + calcService.minus(num1, num2);
+    }
+    @GetMapping(path = "/multiply")
+    public String multiply(@RequestParam int num1, @RequestParam int num2) {
+        return num1 + " * " + num2 + " = " + calcService.multiply(num1, num2);
+    }
+    @GetMapping(path = "/divide")
+    public String divide(@RequestParam int num1, @RequestParam int num2) {
+        return  num1 + " / " + num2 + " = " + calcService.divide(num1, num2);
+
+    }
+
 }
---
+++ src/main/java/ru/calcmax/calculator1/CalcService.java
@@ -0,0 +1,13 @@
+package ru.calcmax.calculator1;
+
+public interface CalcService {
+    String hello();
+
+    String plus(int num1, int num2);
+
+    String minus(int num1, int num2);
+
+    String multiply(int num1, int num2);
+
+    String divide(int num1, int num2);
+}
